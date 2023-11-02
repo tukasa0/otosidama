2c8d2b554e5b27f448035a77106162cd5c872acd
sk2k0 <sk2k0@MSI>
2023-11-02 12:15:16 +0900

    名前変更

---
+++ otosidama.cpp
@@ -0,0 +1,7 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "今年のお年玉は　2万\\ でした。" << endl;
+}
--- 円マーク.cpp
+++
@@ -1,9 +0,0 @@
-﻿#include <iostream>
-using namespace std;
-
-int main()
-{
-	char ch = '\\';
-
-	cout << "今年のお年玉は　2万￥ でした。" << endl;
-}
